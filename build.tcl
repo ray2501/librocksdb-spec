b12d630c846ba8a43e5948833c9afa078f9e6537
Danilo Chang <ray2501@gmail.com>
2019-12-13 21:56:56 +0800

    Update to 6.5.2

--- build.tcl
+++ build.tcl
@@ -1,8 +1,8 @@
 #!/usr/bin/tclsh
 
 set arch "x86_64"
-set base "rocksdb-6.4.6"
-set fileurl "https://github.com/facebook/rocksdb/archive/v6.4.6.tar.gz"
+set base "rocksdb-6.5.2"
+set fileurl "https://github.com/facebook/rocksdb/archive/v6.5.2.tar.gz"
 
 set var [list wget $fileurl -O $base.tar.gz]
 exec >@stdout 2>@stderr {*}$var
